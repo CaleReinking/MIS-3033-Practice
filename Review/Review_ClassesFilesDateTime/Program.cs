a978ecc0eac66f9017b4ef4201fc3d4e077300a1
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-08-31 12:06:42 -0500

    Completed rectangle class and added a date and time

--- Review/Review_ClassesFilesDateTime/Program.cs
+++ Review/Review_ClassesFilesDateTime/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace Review_ClassesFilesDateTime
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {//Classes
+            Rectangle rec1 = new Rectangle();
+            rec1.height = 10;
+            rec1.width = 20;
+
+            Rectangle rec2 = new Rectangle(5, 2);
+
+            Console.WriteLine($"The area of rec1 is {rec1.area()}");
+
+            Console.WriteLine($"The area of rec2 is {rec2.area()}");
+
+           
+            List<Rectangle> rectangles = new List<Rectangle>();
+ //Add existing rectangles to a list
+            rectangles.Add(rec1);
+            rectangles.Add(rec2);
+
+            //Add new rectangle 
+            rectangles.Add(new Rectangle(7, 7));
+
+            foreach (var rec in rectangles)
+            {
+                Console.WriteLine(rec);
+                Console.Write("The rectangle has a width of {rec.width} and hight of {rec.height}");
+                Console.WriteLine($"The area of the rectangle is {rec.area()}");
+            }
         }
     }
 }
---
+++ Review/Review_ClassesFilesDateTime/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Review_ClassesFilesDateTime
+{
+    public class Rectangle
+    {
+        public int width { get; set; }
+        // Can do Width = width in public Rectangle and change this to Width
+        public int height { get; set; }
+
+        public DateTime creationDate { get; set; }
+        
+        public Rectangle()
+        {
+            width = 0;
+            height = 0;
+            //Add creation date
+            creationDate = DateTime.Now;
+        }
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            //Add creation date
+            creationDate = DateTime.Now;
+        }
+        public double area()
+        {
+            return width*height;
+        }
+        public override string ToString()
+        {
+            var diff = DateTime.Now - creationDate;
+            string output = $"The rectangle was created on {creationDate.DayOfWeek} {diff.TotalSeconds}  seconds ago has width of {width} and height of {height}" +
+                $"the area of the rectangle is {area()}";
+            return output;
+        }
+    }
+}
